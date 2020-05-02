b88663a445c14688ceb588110189dae28b5ae175
Soumyo Roy <soumyo78@yahoo.com>
2020-05-03 00:03:22 +0530

    Initial Commit

---
+++ flshcardApplication.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import colorchooser
+
+root = Tk()
+root.title("Flashcard")
+root.geometry("600x400")
+root.iconphoto(True, PhotoImage(file="/home/soumyo/PycharmProjects/FlashcardApp/icon.png"))
+
+
+
+
+
+
+
+root.mainloop()
